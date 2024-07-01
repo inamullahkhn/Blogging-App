030686d541e5ef340540d9859d0ef38ec9118599
hamza5610 <hamzasiddique.idenbrid@gmail.com>
2024-07-01 11:11:46 +0500

    first commit

---
+++ src/app/posts/save/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SavedPots = () => {
+  return <div></div>
+}
+
+export default SavedPots
---
+++ src/app/posts/search/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SearchPost = () => {
+  return <div></div>
+}
+
+export default SearchPost
